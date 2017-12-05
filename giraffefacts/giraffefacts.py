5570977d798db653e9d84c180de38a0781c75974
Tyler <34220819+tylerig@users.noreply.github.com>
2017-12-05 01:04:40 -0500

    Add files via upload

--- giraffefacts/giraffefacts.py
+++ giraffefacts/giraffefacts.py
@@ -2,31 +2,31 @@ import discord
 from discord.ext import commands
 from random import choice as randchoice
 
-class goosefacts:
+class girafffefacts:
 
     def __init__(self, bot):
         self.bot = bot
 
-        self.facts = ["Some geese migrate every year. Others stay in the same place year round.", 
-                "Geese eat seeds, nuts, grass, plants and berries. They love blueberries.",
-                "Geese can live almost anywhere. They like fields, parks and grassy areas near water.", 
-                "Geese fly in a “V” formation. If one goose is injured, other geese will stay with it until it dies or can rejoin the flock.",
-                "Geese are sometimes raised like chickens for their meat or eggs.", 
-                "Male geese protect the nest while the female geese sit on the eggs.", 
-                "Life spans of 15to 25 years!", 
-                "21.7 inches to 43.3 inches in legnth!",
-                "There are around 30 species of goose in the world.", 
-                "Geese vocalise their messages in ten different ways, depending upon the situation. And in a threatening situation, geese stretch out their necks and make loud honks." ,
-                "A day old gosling is capable of diving and swimming as much as 30 to 40 feet underwater. Attaining the age of three months, goslings begin to fly.",
-                "Geese were probably the first type of poultry domesticated by humans, over 3000 years ago in Egypt.",
-                "In another surprising historical use of geese, the first golf balls were stuffed with goose feathers. These balls were handmade and extremely expensive.",
-                "Geese are excellent weeders and during the early days of commercial agriculture goose farmers would supplement their income by renting flocks out to cotton farms for a chemical-free weeding solution.",
-                "In Victorian England geese were a regular companion of the chimney sweep. A goose would be sent down the chimney to collect the built up coal, coming out the other end black with soot."]
+        self.facts = ["Giraffes are the tallest mammals on Earth. Their legs alone are taller than many humans—about 6 feet.", 
+                "They can run as fast as 35 miles an hour over short distances, or cruise at 10 mph over longer distances.",
+                "A giraffe's neck is too short to reach the ground. As a result, it has to awkwardly spread its front legs or kneel to reach the ground for a drink of water.", 
+                "Giraffes only need to drink once every few days. Most of their water comes from all the plants they eat.",
+                "Giraffes spend most of their lives standing up; they even sleep and give birth standing up.", 
+                "The giraffe calf can stand up and walk after about an hour and within a week, it starts to sample vegetation.", 
+                "Despite the females’ attempts to stand over their calves during attacks by lions, spotted hyenas, leopards and African wild dogs (4), many calves are killed in their first few months.", 
+                "A giraffe’s spots are much like human fingerprints. No two individual giraffes have exactly the same pattern.",
+                "Both male and female giraffes have two distinct, hair-covered horns called ossicones. Male giraffes use their horns to sometimes fight with other males.", 
+                "Giraffes only need 5 to 30 minutes of sleep in a 24-hour period! They often achieve that in quick naps that may last only a minute or two at a time." ,
+                "Whilst it was thought that giraffes did not make any sounds, this is now known to be untrue, as giraffes bellow, snort, hiss and make flute-like sounds, as well as low pitch noises beyond the range of human hearing.",
+                "No one has ever seen a giraffe swimming.",
+                "Giraffes only need 5 to 30 minutes of sleep in a 24-hour period.",
+                "Giraffes only need to drink once every few days. Most of their water comes from all the plants they eat.",
+                "A giraffe's spots are like human fingerprints: no two individual giraffes have exactly the same pattern."]
 
     @commands.command()
-    async def goosefact(self):
+    async def giraffefact(self):
         await self.bot.say(randchoice(self.facts))
 
 
 def setup(bot):
-    bot.add_cog(goosefacts(bot))
+    bot.add_cog(giraffefacts(bot))
